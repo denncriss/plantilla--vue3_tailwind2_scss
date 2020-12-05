6bd9f09e7b85c6e442973f2fbf3cb5f4de74831e
denncriss <denncriss.web@gmail.com>
2020-12-05 16:49:00 -0300

    configurado vue-tailwind-sass

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './src/views/About.vue',
+    './src/views/Home.vue',
+    './src/App.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
